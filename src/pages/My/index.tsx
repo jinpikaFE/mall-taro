a86c40f397b93b117b5372ffc6f6232cec7c61e9
jinxin <jinxin@hsmap.com>
2022-06-02 17:21:07 +0800

    feat: 我的页面调整

--- src/pages/My/index.tsx
+++ src/pages/My/index.tsx
@@ -1,71 +1,39 @@
-import { View } from '@tarojs/components';
+import { View, Text } from '@tarojs/components';
 import Taro from '@tarojs/taro';
 import { Observer } from 'mobx-react';
-import { useEffect, useState } from 'react';
-import { AtList, AtListItem } from 'taro-ui';
-import { localUser } from '@/store/user';
+import { AtAvatar, AtButton } from 'taro-ui';
 
 import styles from './index.module.less';
 
 const Index: Taro.FC = () => {
-  const [webUrl, setWebUrl] = useState('');
-  // useEffect(() => {
-  //   Taro.getUserProfile({
-  //     desc: 'used fixd userinfo',
-  //     success: (res) => {
-  //       localUser.setUserInfo(res.userInfo);
-  //     },
-  //   });
-  // }, []);
-
-  /** add study task */
-  const onClickAddTask = () => {
-    Taro.scanCode({
-      success: (res) => {
-        console.log(res);
-      },
-    });
-  };
-
-  /** skip url */
-  const onClickSkip = (url: string) => {
-    Taro.navigateTo({ url: '/pages/WebView/index' });
-  };
-
   return (
     <Observer>
       {() => (
         <View className={styles.container}>
-          <AtList hasBorder={false}>
-            <AtListItem
-              className={styles.userInfo}
-              hasBorder={false}
-              title={localUser?.userInfo?.nickName}
-              thumb={localUser?.userInfo?.avatarUrl}
-              arrow="right"
-              onClick={() =>
-                Taro.navigateTo({ url: '/pages/Login/index' })
-              }
-            />
-          </AtList>
-          <AtList className={styles.actList}>
-            <AtListItem
-              onClick={() => onClickSkip('http://blog.jinxinapp.cn/#/')}
-              title="AuthorGithub"
-              arrow="right"
-            />
-            <AtListItem
-              onClick={() => onClickSkip('https://github.com/jinpikaFE')}
-              title="JinPika Personal Blog"
-              arrow="right"
-            />
-            <AtListItem
-              onClick={onClickAddTask}
-              title="ScanCode"
-              arrow="right"
-            />
-            <AtListItem title="About" arrow="right" />
-          </AtList>
+          <View
+            className={`${styles.userInfoContent}`}
+          >
+            <View
+              className={`at-row at-row__justify--start ${styles.userInfoDesc}`}
+            >
+              <AtAvatar size="small" circle />
+              <View className={styles.userInfoMes}>
+                <View className={`${styles.userInfoText} ${styles.username}`}>
+                  用户名
+                </View>
+                <View className={`${styles.userInfoText} ${styles.isVip}`}>
+                  非会员
+                </View>
+              </View>
+            </View>
+            <View className={`at-row at-row__justify--between at-row__align--center ${styles.memberInfo}`}>
+              <View className={`at-col ${styles.left}`}>会员</View>
+              <View className={`${styles.right}`}>
+                <Text>开通会员省 12312/年</Text>
+                <AtButton className={styles.open} circle>立即开通</AtButton>
+              </View>
+            </View>
+          </View>
         </View>
       )}
     </Observer>
